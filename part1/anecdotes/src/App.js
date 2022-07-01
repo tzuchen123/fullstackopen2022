cfc0f3f1bae0e200d2c8908056fd7aaa668bb162
tzuchenchuang(莊子臻) <tzuchenchuang@msi.com.tw>
2022-07-01 17:40:53 +0800

    part1: courseinfo/unicafe finished  anecdotes unfinished

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(0)
+  const points = { 0: 1, 1: 3, 2: 4, 3: 2 }
+
+  const copy = { ...points }
+  // increment the property 2 value by one
+
+  copy[2] += 1
+  
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes} votes</p>
+      <button onClick={ () => setVotes(votes+1) }>vote</button>
+
+      <button onClick={ () => setSelected(Math.floor(Math.random() * 7)) }>next anecdote</button>
+
+    </div>
+  )
+}
+
+export default App
